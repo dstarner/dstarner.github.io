4bac95bf573756e153eed5ac61eda1558974b620
Daniel Starner <dstarner@bloomberg.net>
2018-10-20 16:06:50 -0400

    Updates

---
+++ precache-manifest.56bc0bd73ea0dd2cc29718096f2c71ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e11f8c478ea3207c310cc7ddddcff550",
+    "url": "/static/media/toaster.e11f8c47.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "07d1a4d3ebf0f91970ad",
+    "url": "/static/js/main.07d1a4d3.chunk.js"
+  },
+  {
+    "revision": "8a2e839b2768f20e23aa",
+    "url": "/static/js/1.8a2e839b.chunk.js"
+  },
+  {
+    "revision": "07d1a4d3ebf0f91970ad",
+    "url": "/static/css/main.101b20e4.chunk.css"
+  },
+  {
+    "revision": "6001562e2f3434325bf055e419ae36f2",
+    "url": "/index.html"
+  }
+];
