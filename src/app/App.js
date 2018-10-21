463a15a6f25cd5c902fcdff82ca1e089111288de
Daniel Starner <dstarner@bloomberg.net>
2018-10-20 20:16:42 -0400

    Dynamic tagline

--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,13 @@ import logo from '../images/toaster.gif';
 import './App.css';
 import Icon from './Icon';
 
+const taglines = [
+  'I adventure and write code',
+  'DevOps & Site Reliability Engineer',
+  'Computer & Nature Lover',
+  'Cars, Computers, & the Outdoors'
+]
+
 const icons = [
   {icon: ['fab', 'instagram'], link: 'https://www.instagram.com/danstarner/'},
   {icon: ['fab', 'twitter'], link: 'https://twitter.com/standarner'},
@@ -16,14 +23,44 @@ const icons = [
 ]
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      timer: null,
+      tagline: this.getTagline(),
+    };
+
+    this.getTagline = this.getTagline.bind(this);
+  }
+
+  componentDidMount() {
+    let timer = setInterval(this.tick.bind(this), 2000);
+    this.setState({timer});
+  }
+
+  componentWillUnmount() {
+    this.clearInterval(this.state.timer);
+  }
+
+  tick() {
+    this.setState({
+      tagline: this.getTagline()
+    });
+  }
+
+  getTagline() {
+    return taglines[Math.floor(Math.random() * taglines.length)];
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 style={{marginBottom: '.3px'}}>Dan Starner</h1>
-          <p>
-            I write code and go on adventures.
+          <p id='tagline'>
+            {this.state.tagline}
           </p>
           <div className='icon-row'>
             {icons.map((icon, index) => {
